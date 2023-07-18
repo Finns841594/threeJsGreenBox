72ecd5f4eab2b52978ec41f58e257b1d4a4e8972
Finns841594 <fengy142857@gmail.com>
2023-07-18 12:59:31 +0200

    Added browser check and readme

---
+++ README.md
@@ -0,0 +1,4 @@
+### three.js tutorial project -- Green Cube
+
+This is a simple project to demonstrate how to use three.js to create a 3D scene with a green cube.
+Run  `npm install` to install the dependencies and `npx vite` to start the server. You will be able to see it at `http://127.0.0.1:5173/`.
--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import WebGL from 'three/addons/capabilities/WebGL.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
@@ -21,4 +22,12 @@ function animate() {
   cube.rotation.y += 0.01;
 	renderer.render( scene, camera );
 }
-animate();
+
+// Check if WebGL is available on current browser
+if ( WebGL.isWebGLAvailable() ) {
+	animate();
+} else {
+	const warning = WebGL.getWebGLErrorMessage();
+	document.getElementById( 'container' ).appendChild( warning );
+
+}
